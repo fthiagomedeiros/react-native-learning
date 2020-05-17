99de266a76863ca8c3d4ec9bc179cc4acc348ac8
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-05-17 02:29:24 -0300

    Clicking Button React Native

--- App.js
+++ App.js
@@ -113,19 +113,63 @@
 //
 // export default App;
 
-import React from 'react';
-import {Text, View} from 'react-native';
+// import React from 'react';
+// import {Text, View} from 'react-native';
+//
+// function App() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}>
+//       <Text>Hello, React!</Text>
+//     </View>
+//   );
+// }
+// export default App;
 
-function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, React!</Text>
-    </View>
-  );
+import React, {Component} from 'react';
+import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
+        <View style={styles.countContainer}>
+          <Text>You clicked {this.state.count} times</Text>
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+
 export default App;
